c4a8a0f2b94a98518a00ad39bff3db4eddc07cf2
sardar awais <khanowais8888@outlook.com>
2021-09-03 00:54:57 +0500

    Add project files.

---
+++ SpeakerKids/SpeakerKids/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SpeakerKids
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
